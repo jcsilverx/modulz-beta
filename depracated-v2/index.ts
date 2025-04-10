8b9c40ae998a3f532c685c578a7f0af047411a01
jcsilverx <jcsilverx@gmail.com>
2025-04-10 08:54:18 -0400

    chore: init

--- depracated-v2/index.ts
+++
@@ -1,8 +0,0 @@
-export type * from "./types";
-
-export * from "./define-pkg-json";
-export * from "./find-pkg-json";
-export * from "./load-pkg-json";
-export * from "./merge-pkg-json";
-
-export * from "./refinements";
--- depracated-v2/refinements/index.ts
+++
@@ -1,9 +0,0 @@
-import { tagTester } from "@zphyrx/fp-core";
-
-const isObject = (value: unknown): value is object =>
-  typeof value === "object" &&
-  value !== null &&
-  !Array.isArray(value) &&
-  tagTester(value, "Object");
-
-export { isObject };
